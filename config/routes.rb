45bbcbf9cd43d551092f41b8c136244f5f932c4d
koki-ok <consideration.days.s@gmail.com>
2022-01-24 17:33:39 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root to: "prototypes#index"
+  resources :users, only: :show
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+end
